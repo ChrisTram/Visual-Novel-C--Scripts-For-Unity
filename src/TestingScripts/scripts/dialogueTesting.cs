676780b17b14c6362a74d2f3dc19170e45888692
Chris <chris.tramier97@gmail.com>
2019-01-17 11:12:15 +0100

    Commit testing scripts

---
+++ src/TestingScripts/scripts/dialogueTesting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogueTesting : MonoBehaviour {
+
+	DialogueSystem ds;
+
+	// Use this for initialization
+	void Start () 
+	{
+		ds = DialogueSystem.instance;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.S)) 
+		{
+			ds.Say ("What dis tang here be?", "Me");
+		}
+	}
+}
